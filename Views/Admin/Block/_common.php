2bf423811087aa49a8b0391d199b1faf09d0ebdf
basic-app <dev@basic-app.com>
2020-01-11 18:27:15 +0300

    n/a

--- Views/Admin/Block/_common.php
+++ Views/Admin/Block/_common.php
@@ -4,7 +4,7 @@ use BasicApp\Helpers\Url;
 
 $title = t('admin.menu', 'Blocks');
 
-$this->data['mainMenu']['blocks']['active'] = true;
+$this->data['mainMenu']['site']['items']['blocks']['active'] = true;
 
 $this->data['breadcrumbs'][] = ['label' => $title, 'url' => Url::createUrl('admin/block')];
 

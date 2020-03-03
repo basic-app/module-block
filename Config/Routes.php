5fd5c431bb359f451e029ed825d3f6abb40cec9b
basic-app <dev@basic-app.com>
2020-03-03 21:13:17 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -5,4 +5,7 @@
  * @link http://basic-app.com
  */
 $routes->add('admin/block', '\BasicApp\Block\Controllers\Admin\Block::index');
+$routes->add('{locale}/admin/block', '\BasicApp\Block\Controllers\Admin\Block::index');
+
 $routes->add('admin/block/(:segment)', '\BasicApp\Block\Controllers\Admin\Block::$1');
+$routes->add('{locale}/admin/block/(:segment)', '\BasicApp\Block\Controllers\Admin\Block::$1');

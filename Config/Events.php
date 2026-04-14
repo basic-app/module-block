257c624e1075df62bf18c365686b19f7c95598aa
basic-app <dev@basic-app.com>
2026-04-14 10:41:56 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -4,7 +4,6 @@
  * @license MIT
  * @link http://basic-app.com
  */
-use BasicApp\Site\SiteEvents;
 use BasicApp\System\SystemEvents;
 use BasicApp\Admin\AdminEvents;
 use BasicApp\Block\Controllers\Admin\Block as BlockController;
@@ -38,31 +37,5 @@ if (class_exists(AdminMenuEvents::class))
             'url'   => Url::createUrl('admin/block'),
             'label' => t('admin.menu', 'Blocks')
         ];
-    });
-}
-
-if (class_exists(SiteEvents::class))
-{
-    SiteEvents::onMainLayout(function($event)
-    {
-        $event->params['copyright'] = $event->params['copyright'] ?? block('site.copyright', '&copy; My Company {year}.');
-        
-        $event->params['siteTitle'] = $event->params['siteTitle'] ?? block('site.title', 'Site Title');
-
-        $event->params['siteName'] = $event->params['siteName'] ?? block('site.name', 'Site Name');
-
-        $event->params['siteDescription'] = $event->params['siteDescription'] ?? block('site.description', 'Site description.');
-    });
-}
-
-if (class_exists(SiteEvents::class))
-{
-    SiteEvents::onRegisterAssets(function($event)
-    {
-        $event->head .= block('site.head');
-        
-        $event->beginBody .= block('site.beginBody');
-        
-        $event->endBody .= block('site.endBody');
     });
 }
